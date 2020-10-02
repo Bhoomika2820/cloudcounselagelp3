5ae3ad802d5e2f137601d762ce2bbb5bdcb559d6
Swagat Ahire <31353464+swagat11@users.noreply.github.com>
2020-10-02 22:55:29 +0530

    make some changes

--- at7.py
+++ at7.py
@@ -5,10 +5,17 @@ Write a Python program to convert temperatures to and from celsius, Fahrenheit.
 Fahrenheit ]
 """
 #Write a Python program to convert temperatures to and from celsius, Fahrenheit
-s=input()
-if s[-1].upper()=="C":
-    f=(9*(int(s[:-2]))//5)+32
-    print(s,"is",f,"in Fahrenheit")
-if s[-1].upper()=="F":
-    c=(5*(int(s[:-2])-32))//9
-    print(s ,"is",c,"in Celsius")
+temp = input("Input the  temperature you like to convert? (e.g., 45F, 102C etc.) : ")
+degree = int(temp[:-1])
+i_convention = temp[-1]
+
+if i_convention.upper() == "C":
+  result = int(round((9 * degree) / 5 + 32))
+  o_convention = "Fahrenheit"
+elif i_convention.upper() == "F":
+  result = int(round((degree - 32) * 5 / 9))
+  o_convention = "Celsius"
+else:
+  print("Input proper convention.")
+  quit()
+print("The temperature in", o_convention, "is", result, "degrees.")
